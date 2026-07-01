84ace5014b4d2ddf3ec8b2f9e36b145ed425e982
Greg Field <greg.d.field@gmail.com>
2026-07-01 11:04:00 -0700

    path correction in main tutorial

--- code/tutorials/matlab_tutorial.m
+++ code/tutorials/matlab_tutorial.m
@@ -14,7 +14,7 @@ help load_data
 
 % you may need to change the path for this to work to something on your
 % local drive
-datarun = load_data('/Volumes/dusom_fieldlab/All_Staff/lab/Experiments/Array/Analysis/2013-11-27-0/data000/data000');
+datarun = load_data('/Volumes/FieldLab/lab/Experiments/Array/Analysis/2013-11-27-0/data000/data000');
 
 % This function called in this way, does not actually load any data, it
 % simply creates a datarun structure in the MATLAB workspace that contains
